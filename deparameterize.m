9855c7d560fee499bc93265beefee87d1d3f3b05
Peng Zhang <pzhang@spawar.navy.mil>
2016-11-29 15:42:40 -0800

    destroy everything except python

--- deparameterize.m
+++
@@ -1,3 +0,0 @@
-function v = deparameterize(v_tilda)
-
-v = [cos(norm(v_tilda,'fro')/2),sinc(norm(v_tilda,'fro')/(2*pi))/2*v_tilda]';
--- minorEig.m
+++
@@ -1,9 +0,0 @@
-function lambda = minorEig(w1,w2,n)
-
-y = zeros(2,2);
-y(1,1) = sum(sum(w1.^2))/n^2;
-y(1,2) = sum(sum(w1.*w2))/n^2;
-y(2,1) = y(1,2);
-y(2,2) = sum(sum(w2.^2))/n^2;
-
-lambda = min(eig(y));
